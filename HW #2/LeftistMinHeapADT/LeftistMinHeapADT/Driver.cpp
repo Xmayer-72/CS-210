854aa27819cb491ba2d9e7a4e42c3dc5c828c158
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2022-03-29 18:08:50 -0500

    Add files via upload

---
+++ HW #2/LeftistMinHeapADT/LeftistMinHeapADT/Driver.cpp
@@ -0,0 +1,106 @@
+#include "LeftistMinHeapADT.cpp"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Given Test Cases:\n**********" << endl;
+
+	//LH1
+	cout << "LH1: Insert 5, 6, 12 and 15\n\n";
+	LeftistHeap<int> lhInt1;
+	lhInt1.insert(5);
+	lhInt1.insert(6);
+	lhInt1.insert(12);
+	lhInt1.insert(15);
+	cout << "LH1:\n" << endl;
+	cout << "Print priority values:" << endl;
+	lhInt1.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt1.showSPL();
+	cout << endl;
+
+
+	//LH2
+	cout << "LH2: Insert 7, 14, 3, 8, and 11\n\n";
+	LeftistHeap<int> lhInt2;
+	lhInt2.insert(7);
+	lhInt2.insert(14);
+	lhInt2.insert(3);
+	lhInt2.insert(8);
+	lhInt2.insert(11);
+	cout << "LH2:\n" << endl;
+	cout << "Print priority values:" << endl;
+	lhInt2.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt2.showSPL();
+	cout << endl;
+
+	cout << "Merge LH1 with LH2\n\n";
+	//merge
+	cout << "Merge(LH1,LH2):\n" << endl;
+	lhInt1.merge(lhInt2);
+	cout << "Print priority values:" << endl;
+	lhInt1.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt1.showSPL();
+	cout << endl;
+
+
+	//LH3
+	cout << "LH3: Insert 77, 22, 9, 68, 16, 34, 13 and 8\n\n";
+	LeftistHeap<int> lhInt3;
+	lhInt3.insert(77);
+	lhInt3.insert(22);
+	lhInt3.insert(9);
+	lhInt3.insert(68);
+	lhInt3.insert(16);
+	lhInt3.insert(34);
+	lhInt3.insert(13);
+	lhInt3.insert(8);
+	cout << "LH3:\n" << endl;
+	cout << "Print priority values:" << endl;
+	lhInt3.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt3.showSPL();
+	cout << endl;
+
+	cout << "DeleteRoot(LH3)\n\n";
+	//deleteMin
+	cout << "DeleteMin(LH3):\n" << endl;
+	lhInt3.deleteMin();
+	cout << "Print priority values:" << endl;
+	lhInt3.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt3.showSPL();
+	cout << endl;
+
+	cout << "DeleteRoot(LH3)\n\n";
+	cout << "DeleteMin(LH3):\n" << endl;
+	lhInt3.deleteMin();
+	cout << "Print priority values:" << endl;
+	lhInt3.showLH();
+	cout << endl;
+	cout << "Print SPL values:" << endl;
+	lhInt3.showSPL();
+	cout << endl;
+
+
+	cout << "Our Test Cases:\n**********" << endl;
+
+
+	// Implement here
+
+
+
+
+
+	system("pause");
+	return 0;
+}
---
+++ HW #2/LeftistMinHeapADT/LeftistMinHeapADT/LeftistMinHeapADT.h
@@ -0,0 +1,42 @@
+#pragma once
+
+
+template <class T>
+class LeftistHeap;
+
+template <class T>
+class LeftistNode
+{
+	T priority;
+	LeftistNode* left;
+	LeftistNode* right;
+	int spl;
+
+	LeftistNode(const T& thePriority, LeftistNode* lt = nullptr, LeftistNode* rt = nullptr, int theSpl = 1) :
+		priority(thePriority), left(lt), right(rt), spl(theSpl)
+	{
+	}
+
+	friend class LeftistHeap<T>;
+};
+
+template <class T>
+class LeftistHeap
+{
+public:
+	LeftistHeap();
+
+	void insert(const T& x);
+	void deleteMin();
+	void merge(LeftistHeap& rhs);
+	void showLH() const;
+	void showSPL() const;
+
+private:
+	LeftistNode<T>* root;
+	LeftistNode<T>* merge1(LeftistNode<T>* h1, LeftistNode<T>* h2) const;
+	LeftistNode<T>* merge2(LeftistNode<T>* h1, LeftistNode<T>* h2) const;
+	void swapChildren(LeftistNode<T>* t) const;
+	void showLHHelper(LeftistNode<T>* p, int level) const;
+	void showSPLHelper(LeftistNode<T>* p, int level) const;
+}; #pragma once
