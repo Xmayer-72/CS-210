cee585f7431684fc01f1f5fd8584cfb83f4353d0
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2022-04-14 20:07:44 -0500

    Update QuickSelect_and_QuickSort.h
    This is what I've got for Quick Select so far, it compiles but the output is off. I'm trying to figure out why.

--- HW #3/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort.h
+++ HW #3/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort.h
@@ -6,7 +6,26 @@ using namespace std;
 template <class T, int N>
 void partition(int min, int max, int& pivotpoint, array<T, N>& arr)
 {
-	// Implement here
+	int i = 0;
+	int j = 0;
+	int pivotItem = 0;
+
+	pivotItem = arr[min];
+	j = min;
+	for (i = min + 1; i <= max; i++) {
+		if (arr[i] < pivotItem) {
+			j++;
+			/*array<T, N> ary1[] = arr[i];
+			arr[i] = arr[j];
+			arr[j] = ary1[];*/
+			std::swap(arr[i], arr[j]);
+		}
+		pivotpoint = j;
+		/*array<T, N> ary2[] = arr[min];
+		arr[min] = ary[pivotpoint];
+		arr[pivotpoint] = ary2[];*/
+		std::swap(arr[min], arr[pivotpoint]);
+	}
 }
 
 //-------------------------------------
@@ -16,8 +35,22 @@ void partition(int min, int max, int& pivotpoint, array<T, N>& arr)
 template <class T, int N>
 T Qselection(int min, int max, int k, array<T, N>& arr)
 {
-	// Implement here
-}
+	int pivot = 0;
+	if (min == max) {
+		return arr[min];
+	}
+	else {
+		partition(min, max, pivot, arr);
+		if (k == pivot) {
+			return arr[pivot];
+		}
+		else if (k < pivot) {
+			return Qselection(min, pivot - 1, k, arr);
+		}
+		else {
+			return Qselection(pivot + 1, max, k, arr);
+		}
+	}
 
 //k represents the kth smallest value, 
 
