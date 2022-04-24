4de9b1a37c8ced4163c75f00a71c7271eb56bd65
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2022-04-23 22:54:58 -0500

    Update AVLSearchTreeADT.cpp
    Doesn't compile, I think the logic is correct. The syntax is probably wrong in some places.

--- HW #3/AVL Search Tree/AVLSearchTree/AVLSearchTreeADT.cpp
+++ HW #3/AVL Search Tree/AVLSearchTree/AVLSearchTreeADT.cpp
@@ -6,43 +6,75 @@ using namespace std;
 template<class T>
 int AVLNode<T>::getHeight() const
 {
-	// Implement here!
+	return height;
 }
 template<class T>
 void AVLNode<T>::setHeight(int newHeight)
 {
-	// Implement here!
+	height = newHeight;
 }
 //calculate the balance factor for this node, based on its children's heights
 template<class T>
 int AVLNode<T>::calculateBalanceFactor()
 {
-	// Implement here!
+	AVLNode<T> currentNode = new AVLNode<T>();
+	int bf = ((currentNode.left->getHeight()) - (currentNode.right->getHeight()));
+	return bf;
 }
 template<class T>
 AVLTree<T>::AVLTree()
 {
-	// Implement here!
+	root = NULL;
 }
 //insert the element x into the AVL Tree
 template<class T>
 void AVLTree<T>::insert(const T& x)
 {
-	// Implement here!
+	AVLNode<T>* n = new AVLNode<T>;
+	insertHelper(n, x);
 }
 //return root node
 //helper function for insertElement
 template<class T>
 AVLNode<T>* AVLTree<T>::insertHelper(AVLNode<T>* node, const T& x)
 {
-	// Implement here!
+	if (getRoot() == NULL) {
+		AVLNode<T>::AVLNode(x);
+	}
+	if (x > node->element) {
+		node->right = insertHelper(node->right, x);
+	} else if (x < node->element) {
+		node->left = insertHelper(node->left, x);
+	}
+	else {
+		return getRoot();
+	}
+
+	node->setHeight(1 + max(node->left->getHeight(), node->right->getHeight()));
+
+	int balance = node->calculateBalanceFactor();
+
+	if (balance > 1 && x < node->left->element) {
+		LLRotateWithLeftChild(node);
+	}
+	if (balance < -1 && x < node->right->element) {
+		RRRotateWithRightChild(node);
+	}
+	if (balance > 1 && x > node->left->element) {
+		LRDoubleWithLeftChild(node);
+	}
+	if (balance < -1 && x > node->right->element) {
+		RLDoubleWithRightChild(node);
+	}
+	return getRoot();
 }
 //outputs the keys, rotated CCW 90 degrees
 //using reverse in-order traversal
 template<class T>
 void AVLTree<T>::showAvlST() const
 {
-	// Implement here!
+	AVLNode<T>* n = new AVLNode<T>;
+	showAvlSTHelper(n, n->getHeight());
 }
 //recursive helper for showAvlST
 //outputs the subtree with root at p
@@ -50,7 +82,18 @@ void AVLTree<T>::showAvlST() const
 template<class T>
 void AVLTree<T>::showAvlSTHelper(AVLNode<T>* p, int level) const
 {
-	// Implement here!
+	int i = p->getHeight();
+	for (i; i > 0; --i) {
+		showAvlSTHelper(p->right, i);
+		cout << p->element << " ";
+	}
+	showAvlSTHelper(getRoot(), 0);
+	cout << p->element << " ";
+
+	for (i; i > 0; --i) {
+		showAvlSTHelper(p->left, i);
+		cout << p->element << " ";
+	}
 }
 //outputs the balance factors, rotated CCW 90 degrees
 //using reverse in-order traversal
@@ -73,8 +116,8 @@ void AVLTree<T>::LLRotateWithLeftChild(AVLNode<T>*& t) const
 	AVLNode<T> *t1 = t->left;
 	t->left = t1->right;
 	t1->right = t;
-	t->height = max(getHeight(t->left), getHeight(t->right)) + 1;
-	t1->height = max(getHeight(t1->left), getHeight(t1->right)) + 1;
+	t->setHeight( max(t->getHeight(), t->getHeight()) + 1);
+	t1->setHeight( max(t1->getHeight(), t1->getHeight()) + 1);
 	t = t1;
 }
 template<class T>
@@ -83,8 +126,8 @@ void AVLTree<T>::RRRotateWithRightChild(AVLNode<T>*& t) const
 	AVLNode<T>* t1 = t->right;
 	t->right = t1->right;
 	t1->left = t;
-	t->height = max(getHeight(t->left), getHeight(t->right)) + 1;
-	t1->height = max(getHeight(t1->left), getHeight(t1->right)) + 1;
+	t->height = max(t->getHeight(), t->getHeight()) + 1;
+	t1->height = max(t1->getHeight(), t1->getHeight()) + 1;
 	t = t1;
 }
 template<class T>
@@ -104,10 +147,10 @@ void AVLTree<T>::RLDoubleWithRightChild(AVLNode<T>*& t) const
 template<class T>
 AVLNode<T>* AVLTree<T>::getRoot() const //added function to access the root
 {
-	// Implement here!
+	return root;
 }
 template<class T>
 void AVLTree<T>::setRoot(AVLNode<T>* newRoot)
 {
-	// Implement here!
+	root = newRoot;
 }
