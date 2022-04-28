82473239bcb610f05c92ac6fdba3de3d463af269
SoloShemp <100331830+SoloShemp@users.noreply.github.com>
2022-04-27 22:46:32 -0500

    Update Driver.cpp

--- HW #3/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort/Driver.cpp
+++ HW #3/QuickSelect_and_QuickSort/QuickSelect_and_QuickSort/Driver.cpp
@@ -26,9 +26,24 @@ void QuickSelectTest()
 	cout << "\n\n";
 	cout << "2. Our Test Cases:\n";
 
+	array<int, 15> TestCaseArray2 = { 4, 1, 5, 8, 12, 6, 7, 10, 13, 30, 44, 14, 15,
+	18, 22 };
+	cout << "\nInput Array: \n\n";
+	printArray(TestCaseArray2);
+	cout << "\n\n";
+	int fourthSmallest2 = QuickSelect(4, TestCaseArray2);
+	cout << "\n\n";
+	cout << "The 4th Smallest item is: " << fourthSmallest2;
+	int eigthSmallest2 = QuickSelect(8, TestCaseArray2);
+	cout << "\n\n";
+	cout << "The 8th Smallest item is: " << eigthSmallest2;
+	int twelfthSmallest2 = QuickSelect(12, TestCaseArray2);
+	cout << "\n\n";
+	cout << "The 12th Smallest item is: " << twelfthSmallest2;
+	cout << "\n\n";
+	cout << "\n\n";
 
-
-	// Implement here
+	
 
 
 }
@@ -52,8 +67,17 @@ void QuickSortTest()
 	cout << " 2. Our Test Cases:\n";
 
 
-
-	// Implement here
+	array<int, 15> TestCaseArray2 = { 8, 3, 5, 11, 12, 1, 7, 10, 13, 30, 36, 14, 15,
+	28, 22 };
+	cout << "\nInput Array: \n\n";
+	printArray(TestCaseArray2);
+	cout << "\n\n";
+	QuickSort(TestCaseArray2);
+	cout << "\n\nSorted Array: \n\n";
+	printArray(TestCaseArray2);
+	cout << "\n\n";
+	cout << "\n\n";
+	
 
 
 }
